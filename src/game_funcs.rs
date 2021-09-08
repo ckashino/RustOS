51ed76325dcd8428e1c792be3534272c887d0718
crashino <cjkashino@gmail.com>
2021-09-08 14:03:11 -0700

    Redid Printing Functions
    redid printing functions. added newline & clearline

---
+++ src/game_funcs.rs
@@ -0,0 +1,11 @@
+mod vga_buffer;
+
+use vga_buffer::write;
+use vga_buffer::Color;
+
+
+pub fn print_board(){
+
+
+
+}
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,7 @@
+use core::fmt;
+use volatile::Volatile;
+
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -24,6 +28,10 @@ pub enum Color {
 #[repr(transparent)]
 struct ColorCode(u8);
 
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
 impl ColorCode {
     fn new(foreground: Color, background: Color) -> ColorCode{
 
@@ -45,11 +53,11 @@ struct ScreenChar {
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
-#[repr(transparent)]
+/* #[repr(transparent)]
 struct Buffer {
     chars:[[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 
-}
+} */
 
 pub struct Writer {
     column_position: usize,
@@ -70,11 +78,11 @@ impl Writer {
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
 
-                };
+                });
                 self.column_position += 1
             }
 
@@ -89,21 +97,55 @@ impl Writer {
                 }
             }
         }
-        fn new_line(&mut self) {
-        }
     
+}
+impl Writer {
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
 
 pub fn print_something() {
+    use core::fmt::Write;
+
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     };
 
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+
+
+    
+    writer.new_line();
+
+
 }
+
+
